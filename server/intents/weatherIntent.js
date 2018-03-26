f6d9a3399a15428b231f729ee1646ab94d2476d5
oriassaraf <ori.assaraf@gmail.com>
2018-03-26 17:30:36 +0300

    Add Weather Micro Service

---
+++ server/intents/weatherIntent.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const request = require('superagent');
+
+module.exports.process = function process(intentData, registry, cb) {
+    if (intentData.intent[0].value !== 'weather')
+        return cb(new Error(`Expected weather intent, got ${intentData.intent[0].value}`));
+
+    if (!intentData.location) return cb(new Error('Missing location in weather intent'));
+
+    const location = intentData.location[0].value.replace(/,.?iris/i,'');
+
+    const service = registry.get('weather');
+    if (!service) {
+        console.log('No service avilable');
+        return cb(false, 'No service avilable');
+    }
+
+    request.get(`http://${service.ip}:${service.port}/service/${location}`, (err, res) => {
+        if(err || res.statusCode != 200 || !res.body.result) {
+            console.log('error call iris-weather')
+            console.log(err);
+            return cb(false, `I had a problem finding out the weather in ${location} `);
+        }
+
+        return cb(false, `The current weather in In ${location} is ${res.body.result}`);
+    })
+}
